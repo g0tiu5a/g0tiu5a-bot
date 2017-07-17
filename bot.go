231c9459f5ed820fd256087113718be7ebb51137
Ryosuke Sato <rskjtwp@gmail.com>
2017-07-17 16:25:31 +0900

    Add cocoa

--- bot.go
+++ bot.go
@@ -6,7 +6,13 @@ import (
 	"os"
 	"strings"
 
+	"github.com/g0tiu5a/g0tiu5a-bot/ctftime"
 	"github.com/nlopes/slack"
+)
+
+const (
+	botName = "保登 心愛"
+	botIcon = ":cocoa-1:"
 )
 
 type Bot struct {
@@ -17,12 +23,74 @@ type Bot struct {
 func (bot *Bot) handleResponse(ev *slack.MessageEvent) error {
 	command := strings.Fields(ev.Text)
 
+	if len(command) <= 0 {
+		return nil
+	}
+
 	switch command[0] {
 	case "ping":
 		bot.rtm.SendMessage(bot.rtm.NewOutgoingMessage("pong", ev.Channel))
 		return nil
+
+	case "ctftime":
+		err := bot.ctftime(command, ev.Channel)
+		return err
 	}
 	return nil
+}
+
+func (bot *Bot) ctftime(commands []string, channel string) error {
+	if len(commands) != 2 {
+		return nil
+	}
+
+	var attachments []slack.Attachment
+	const timeLayout = "2006/01/02 15:04:05 UTC"
+
+	switch commands[1] {
+	case "event":
+		events := ctftime.GetAPIData()
+		for _, event := range events {
+			attachment := slack.Attachment{
+				Color:     "#F35A00",
+				Title:     event.Title,
+				TitleLink: event.Url,
+				Fields: []slack.AttachmentField{
+					{
+						Title: "format",
+						Value: event.Format,
+						Short: true,
+					},
+					{
+						Title: "weight",
+						Value: fmt.Sprintf("%f", event.Weight),
+						Short: true,
+					},
+					{
+						Title: "start",
+						Value: event.Start.Format(timeLayout),
+						Short: true,
+					},
+					{
+						Title: "finish",
+						Value: event.Finish.Format(timeLayout),
+						Short: true,
+					},
+				},
+			}
+
+			attachments = append(attachments, attachment)
+		}
+	}
+
+	params := slack.PostMessageParameters{
+		Attachments: attachments,
+		Username:    botName,
+		IconEmoji:   botIcon,
+	}
+
+	_, _, err := bot.api.PostMessage(channel, "", params)
+	return err
 }
 
 func (bot *Bot) run() int {
